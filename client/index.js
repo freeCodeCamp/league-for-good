ec16c3dd851c9911dde7639ba7ebdf0eea7282b0
ahstein3521 <adamhs3521@gmail.com>
2017-05-30 12:18:25 -0400

    working solution for dynamic theme

--- client/index.js
+++ client/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, getState, subscribe } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './src/reducers/index';
 
@@ -13,7 +11,7 @@ import App from './src/app.jsx';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import themes from './src/components/themes';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
+
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 // This must be invoked in order for the Material-UI theme provider to work
@@ -22,36 +20,31 @@ injectTapEventPlugin();
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
-function select(state) {
-	return state.theme;
-}
+// function select(state) {
+// 	return state.theme;
+// }
 
-let currentTheme = themes.getCurrentThemeName();
-let muiTheme = getMuiTheme({
-	palette: themes.getThemeList()[themes.getCurrentThemeName()],
-});
+// let currentTheme = themes.getCurrentThemeName();
+// let palette = themes.getThemeList()[themes.getCurrentThemeName()];
 
-function handleThemeChange() {
-	let previousTheme = currentTheme;
-	currentTheme = select(store.getState());
+// function handleThemeChange() {
+// 	let previousTheme = currentTheme;
+// 	currentTheme = select(store.getState());
 
-	if (previousTheme !== currentTheme) {
-		muiTheme = getMuiTheme({
-			palette: themes.getThemeList()[currentTheme],
-		});
-	}
-}
-console.log('muitheme', muiTheme);
+// 	if (previousTheme !== currentTheme) {
+		
+// 		palette = themes.getThemeList()[currentTheme]
+		
+// 	}
+// }
 
-store.subscribe(handleThemeChange);
-
+// store.subscribe(handleThemeChange);
 
 const Root = () => {
+
 	return(
 		<Provider store={store}>
-			<MuiThemeProvider muiTheme={muiTheme}>
-				<App />
-			</MuiThemeProvider>
+			<App />
 		</Provider>
 	);
 };
