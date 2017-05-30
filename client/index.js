064c91ea58b11928844dcd5b6a6b828e2ab85ba4
ahstein3521 <adamhs3521@gmail.com>
2017-05-30 01:12:36 -0400

    update theme in subscribe cb

--- client/index.js
+++ client/index.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { getState, subscribe } from 'redux';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
+
+
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, getState, subscribe } from 'redux';
 import thunk from 'redux-thunk';
-
 import reducers from './src/reducers/index';
 
 import App from './src/app.jsx';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import themes from './src/components/themes';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
 // This must be invoked in order for the Material-UI theme provider to work
 injectTapEventPlugin();
+
 
 const store = createStore(reducers, applyMiddleware(thunk));
 
