d83565c25b68c92af7159e151b814bfefd4d0e80
makkoli <makkoli@vt.edu>
2017-07-10 15:17:02 -0400

    fixed rendering of deletion of a staff email
    Delete now removes the staff email from the list of all staff

--- client/src/reducers/settings.js
+++ client/src/reducers/settings.js
@@ -12,21 +12,21 @@ import {
  */
 
 function removeStaff({removedStaffEmail}) {
-	return staffEmail => removedStaffEmail !== staffEmail;
+	return staff=> removedStaffEmail !== staff.email;
 }
 
 export default function(state = {}, action) {
 	switch (action.type) {
 
-	case SELECT_STAFF_MEMBERS:
-		return { ...state, staff: action.staff };
-	case CREATE_STAFF_MEMBER:
-		return { ...state, staff: [action.newStaff, ...state.staff] };
-	case REMOVE_STAFF_MEMBER:
-		return { ...state, staff: state.staff.filter(removeStaff(action)) };
-	case UPDATE_STAFF_MEMBER:
-		return state;
-	default:
-		return state;
+		case SELECT_STAFF_MEMBERS:
+			return { ...state, staff: action.staff };
+		case CREATE_STAFF_MEMBER:
+			return { ...state, staff: [action.newStaff, ...state.staff] };
+		case REMOVE_STAFF_MEMBER:
+			return { ...state, staff: state.staff.filter(removeStaff(action)) };
+		case UPDATE_STAFF_MEMBER:
+			return state;
+		default:
+			return state;
 	}
 }
