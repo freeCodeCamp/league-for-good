2425124bfabf32038e6b74b900e6382a980e7cdb
makkoli <makkoli@vt.edu>
2017-07-10 16:13:57 -0400

    fix in the init script
    fixed linter errors

--- init/initRoles.js
+++ init/initRoles.js
@@ -1,145 +1,121 @@
-// /*
-//  * This script will initialize the default roles in the database for the app
-//  * This is a sample of roles that you can use, feel free to create your own.
-//  *
-//  * Default roles:
-//  * 	owner:
-//  * 		-owner of the league
-//  * 		-has access to all privileges
-//  *	manager:
-//  *		-manager of the league
-//  *		-has access to all the teams, players, and season privileges
-//  *		-no access to settings tab
-//  *	coach:
-//  *		-coach of team(s)
-//  *		-has access to the teams tab
-//  *		-no access to the players, season, or settings tab
-//  *		-can view a subset of team(s) that he is assigned to and their roster(s)
-//  *		-can view player information through the roster
-//  *
-//  * A description of what each privilege
-// does can be found in ../models/roles.js
-//  */
+require('dotenv').config();
+require('../server/models/index');
 
-// const mongoose = require('mongoose');
-// require('dotenv').config();
-// <<<<<<< HEAD:init/initRoles.js
-// const MONGO_URI = process.env.MONGO_URI;
-// const Role = require('../server/models/roles.js');
-// =======
-
-// const MONGO_URI = process.env.MONGO_URI;
-// const Role = require('../models/roles.js');
-// >>>>>>> 2b3f020ce568d018cde22a5fad6e24be422578e2:server/init/initRoles.js
+/*
+ * This script will initialize the default roles in the database for the app
+ * This is a sample of roles that you can use, feel free to create your own.
+ *
+ * Default roles:
+ * 	owner:
+ * 		-owner of the league
+ * 		-has access to all privileges
+ *	manager:
+ *		-manager of the league
+ *		-has access to all the teams, players, and season privileges
+ *		-no access to settings tab
+ *	coach:
+ *		-coach of team(s)
+ *		-has access to the teams tab
+ *		-no access to the players, season, or settings tab
+ *		-can view a subset of team(s) that he is assigned to and their roster(s)
+ *		-can view player information through the roster
+ *
+ * A description of what each privilege does can be found in ../models/roles.js
+ */
 
 
-// mongoose.connect(MONGO_URI);
-// mongoose.connection
-//     .once('open', () => console.log('Connected to MongoDB'))
-//     .on('error', error => console.log(error));
+const mongoose = require('mongoose');
+
+const Role = mongoose.model('role');
+const MONGO_URI = process.env.MONGO_URI;
 
 
-// <<<<<<< HEAD:init/initRoles.js
-// const adminRole = new Role({
-// 	title: 'Administrator',
-//       	privileges: {
-// =======
-// const ownerRole = new Role({
-// 	name: 'owner',
-// 		privileges: {
-// >>>>>>> 2b3f020ce568d018cde22a5fad6e24be422578e2:server/init/initRoles.js
-// 		viewTeams: true,
-// 		viewSubsetTeams: false,
-// 		createTeams: true,
-// 		editTeams: true,
-// 		deleteTeams: true,
-// 		viewPlayers: true,
-// 		viewPlayerRegistrations: true,
-// 		createPlayers: true,
-// 		editPlayers: true,
-// 		assignPlayers: true,
-// 		viewSeasons: true,
-// 		editSeasons: true,
-// 		deleteSeasons: true,
-// 		viewSettings: true,
-// 		createStaff: true,
-// 		editStaff: true,
-// 		deleteStaff: true,
-// <<<<<<< HEAD:init/initRoles.js
-// 		deleteLeague: false,
-// 	},
-// });
-
-// const managerRole = new Role({
-// 	title: 'Manager',
-//       	privileges: {
-// =======
-// 		deleteLeague: true
-// 	}
-// });
-
-// const managerRole = new Role({
-// 	name: 'manager',
-// 		privileges: {
-// >>>>>>> 2b3f020ce568d018cde22a5fad6e24be422578e2:server/init/initRoles.js
-// 		viewTeams: true,
-// 		viewSubsetTeams: false,
-// 		createTeams: true,
-// 		editTeams: true,
-// 		deleteTeams: true,
-// 		viewPlayers: true,
-// 		viewPlayerRegistrations: true,
-// 		createPlayers: true,
-// 		editPlayers: true,
-// 		assignPlayers: true,
-// 		viewSeasons: true,
-// 		editSeasons: true,
-// 		deleteSeasons: true,
-// 		viewSettings: false,
-// 		createStaff: false,
-// 		editStaff: false,
-// 		deleteStaff: false,
-// 		deleteLeague: false
-// 	}
-// });
-
-// const coachRole = new Role({
-// <<<<<<< HEAD:init/initRoles.js
-// 	title: 'Coach',
-//       	privileges: {
-// =======
-// 	name: 'coach',
-// 		privileges: {
-// >>>>>>> 2b3f020ce568d018cde22a5fad6e24be422578e2:server/init/initRoles.js
-// 		viewTeams: false,
-// 		viewSubsetTeams: true,
-// 		createTeams: false,
-// 		editTeams: false,
-// 		deleteTeams: false,
-// 		viewPlayers: false,
-// 		viewPlayerRegistrations: false,
-// 		createPlayers: false,
-// 		editPlayers: false,
-// 		assignPlayers: false,
-// 		viewSeasons: false,
-// 		editSeasons: false,
-// 		deleteSeasons: false,
-// 		viewSettings: false,
-// 		createStaff: false,
-// 		editStaff: false,
-// 		deleteStaff: false,
-// 		deleteLeague: false
-// 	}
-// });
+mongoose.connect(MONGO_URI);
+mongoose.connection
+    .once('open', () => console.log('Connected to MongoDB'))
+    .on('error', error => console.log(error));
 
 
-// const adminPromise = adminRole.save()
-// 	.then(() => console.log('Owner role added'));
-// const managerPromise = managerRole.save()
-// 	.then(() => console.log('Manager role added'));
-// const coachPromise = coachRole.save()
-// 	.then(() => console.log('Coach role added'));
+const adminRole = new Role({
+	title: 'Administrator',
+	privileges: {
+		viewTeams: true,
+		viewSubsetTeams: false,
+		createTeams: true,
+		editTeams: true,
+		deleteTeams: true,
+		viewPlayers: true,
+		viewPlayerRegistrations: true,
+		createPlayers: true,
+		editPlayers: true,
+		assignPlayers: true,
+		viewSeasons: true,
+		editSeasons: true,
+		deleteSeasons: true,
+		viewSettings: true,
+		createStaff: true,
+		editStaff: true,
+		deleteStaff: true,
+		deleteLeague: false
+	}
+});
 
-// Promise.all([adminPromise, managerPromise, coachPromise])
-// 	.then(() => console.log('Roles added'))
-// 	.catch(err => console.log(err));
+const managerRole = new Role({
+	title: 'General Manager',
+	privileges: {
+		viewTeams: true,
+		viewSubsetTeams: false,
+		createTeams: true,
+		editTeams: true,
+		deleteTeams: true,
+		viewPlayers: true,
+		viewPlayerRegistrations: true,
+		createPlayers: true,
+		editPlayers: true,
+		assignPlayers: true,
+		viewSeasons: true,
+		editSeasons: true,
+		deleteSeasons: true,
+		viewSettings: false,
+		createStaff: false,
+		editStaff: false,
+		deleteStaff: false,
+		deleteLeague: false
+	}
+});
+
+const coachRole = new Role({
+	title: 'Coach',
+	privileges: {
+		viewTeams: false,
+		viewSubsetTeams: true,
+		createTeams: false,
+		editTeams: false,
+		deleteTeams: false,
+		viewPlayers: false,
+		viewPlayerRegistrations: false,
+		createPlayers: false,
+		editPlayers: false,
+		assignPlayers: false,
+		viewSeasons: false,
+		editSeasons: false,
+		deleteSeasons: false,
+		viewSettings: false,
+		createStaff: false,
+		editStaff: false,
+		deleteStaff: false,
+		deleteLeague: false
+	}
+});
+
+
+const adminPromise = adminRole.save()
+	.then(() => console.log('Owner role added'));
+const managerPromise = managerRole.save()
+	.then(() => console.log('General Manager role added'));
+const coachPromise = coachRole.save()
+	.then(() => console.log('Coach role added'));
+
+Promise.all([adminPromise, managerPromise, coachPromise])
+	.then(() => console.log('Roles added'))
+	.catch(err => console.log(err));
