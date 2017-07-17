8f673c27afadcd20e9c23f5fd63e04958b98c238
ahstein3521 <adamhs3521@gmail.com>
2017-07-17 16:31:02 -0400

    table template handles pagination

---
+++ client/src/components/dashboard/helper/tableTemplate/tableHeader.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {
+	TableHeader as MuiTableHeader,
+	TableHeaderColumn,
+	TableRow
+} from 'material-ui/Table';
+import ColumnHeaderChild from './columnHeaderChild.jsx';
+import { cssDashboard } from '../../../style';
+
+// Header row for the table containing column names
+export default class TableHeader extends Component {
+	static muiName = 'TableHeader';
+
+	
+	renderColumns = () => {
+		const { onSort, sortColumnIndex, sortDirection } = this.props;
+		return this.props.headers.map(function(header, i) {
+			return (
+				<TableHeaderColumn
+					colSpan={header.colSpan || 1}
+					key={i}
+					style={cssDashboard.table.colHeaderStyle}
+					>
+					<ColumnHeaderChild
+						colIndex={i}
+						label={header.label}
+						onClick={onSort}
+						sortable={header.sortable}
+						sortColumnIndex={sortColumnIndex}
+						sortDirection={sortDirection}
+					/>
+				</TableHeaderColumn>
+			);
+		});
+	}
+	render() {
+		return (
+			<MuiTableHeader
+				adjustForCheckbox={false}
+				displaySelectAll={false}
+				selectable={false}
+				>
+				<TableRow>
+					{ this.renderColumns()}
+				</TableRow>
+			</MuiTableHeader>
+		);
+	}
+}
+
+TableHeader.propTypes = {
+	headers: PropTypes.arrayOf(PropTypes.object),
+	onSort: PropTypes.func,
+	sortColumnIndex: PropTypes.number,
+	sortDirection: PropTypes.string
+};
