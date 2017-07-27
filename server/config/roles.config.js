29246bd403463ce42057e1be2f274cd37695274b
makkoli <makkoli@vt.edu>
2017-07-27 12:57:36 -0400

    Docs/added docs to roles (#35)
    * Moved roles from a script to config file
    
    Roles has been moved from a script file that would insert roles into the
    db to a config file on the server side. This makes more sense since
    there will be no custom roles. All backend requests were modified for
    this change.
    
    * fixed testing errors
    
    * idk
    
    * Added missing role documentation
    
    Accidentally deleted the privilege description documentation so I re-added it.

--- server/config/roles.config.js
+++ server/config/roles.config.js
@@ -20,6 +20,29 @@
  * A description of what each privilege does can be found in ../models/roles.js
  */
 
+
+// Privileges that each role can be assigned
+//
+// Teams
+// 	viewTeams: user can view the team tab and a list of all teams
+// 	viewSubsetTeams: user can view the team tab and only a subset of teams
+// 			 the list of teams is defined in the staff object in
+// 			 the league collection
+// 			 this would only be used for users that need to have limited
+// 			 access to teams such as coaches or assistant coaches
+// 	createTeams: user can create a team
+// 	editTeams: user can edit a team
+// 	deleteTeams: user can delete teams
+//
+// Players
+// 	viewPlayers: user can view the player tab and a list of players
+// 	viewPlayerRegistrations: user can view player applicants to the league
+// 				 also allows the user to accept and deny applicants
+//	createPlayers: user can create players
+//	editPlayers: user can edit players
+//	assignPlayers: user can assign players to teams
+//
+
 // This role is the one that is given to the creator of a league
 const ownerRole = 'Administrator';
 
