6ab44528d95f0620fd4a5347474480e8c6952c59
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-02-07 13:35:47 -0500

    Added edit button to interface. Working on form for input

---
+++ client/src/components/dashboard/settings/forms/editLeageNameForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class EditLeageNameForm extends Component {
+    render() {
+        return (
+            <div><h2>Hello, World!</h2></div>
+        );
+    }
+}
