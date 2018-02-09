24fa458baba1081bb241cad8c69ce88e6f10e44d
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-02-09 15:25:12 -0500

    Fixed form layout and added route for changing the name

--- client/src/components/dashboard/settings/forms/editLeageNameForm.jsx
+++ client/src/components/dashboard/settings/forms/editLeageNameForm.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { TextField, SelectField } from 'redux-form-material-ui';
 
-export default class EditLeageNameForm extends Component {
-    render() {
-        return (
-            <div><h2>Hello, World!</h2></div>
-        );
-    }
-}
+import { cssContent, cssDashboard } from '../../../styles';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const EditLeageNameForm = props => {
+
+    const { handleSubmit } = props;
+    
+    return (
+        <div style={cssContent.body}>
+            <h1 style={cssDashboard.title}>Enter New League Name</h1>
+            <form style={cssDashboard.form}>
+            <Field
+                component={TextField}
+                floatingLabelText='Enter New League Name'
+                hintText='New Name here'
+                name='leagueName'
+                style={cssDashboard.settings.forms.add.textField}
+            />
+            <RaisedButton
+                backgroundColor={cssDashboard.raisedButton.backgroundColor}
+                label='Change League Name'
+                labelStyle={cssDashboard.raisedButton.label}
+                style={cssDashboard.raisedButton.style}
+                type='submit'
+            />
+            </form>
+        </div>
+    );
+};
+
+export default reduxForm({
+    form: 'EditLeagueNameForm'
+})( EditLeageNameForm );
