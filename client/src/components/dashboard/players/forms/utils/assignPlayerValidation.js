398b8411e5d99c7d12bc87ca1459a22af9e9c6b0
IRGames <isaac.w.raymond@gmail.com>
2018-04-11 14:03:24 -0400

    assign player form validation

--- client/src/components/dashboard/players/forms/utils/assignPlayerValidation.js
+++ client/src/components/dashboard/players/forms/utils/assignPlayerValidation.js
@@ -1,18 +1,19 @@
+const jerseyNumRegrex = new RegExp('^([1-9][0-9]?)$', 'i');
 
 export default function(val) {
-	const errors = {};
+	const errors = { team: {} };
 
 	if (!val.playerId) {
 		errors.playerId = 'Please select a player';
+	} else if (!val.team) {
+		errors.team.teamId = 'Please select a team';
+	} else if (val.team && !val.team.position) {
+		errors.team.position = 'Please choose one or more position';
+	} else if (val.team && !val.team.jerseyNum) {
+		errors.team.jerseyNum = 'Please select a jersey number';
+	} else if (!jerseyNumRegrex.test(val.team.jerseyNum)) {
+		errors.team.jerseyNum = 'Jersey numbers can be between 1 and 99';
 	}
-	if (!val.teamId) {
-		errors.teamId = 'Please select a team';
-	}
-	if (!val.position) {
-		errors.position = 'Please choose one or more position';
-	}
-	if (!val.jerseyNum ) {
-		errors.jerseyNum = 'Please select a jersey number';
-	}
+
 	return errors;
 }
