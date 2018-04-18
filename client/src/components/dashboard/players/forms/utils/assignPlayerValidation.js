ce4a56168c7cae43cb697074684136eff417dea2
IRGames <isaac.w.raymond@gmail.com>
2018-04-17 21:57:43 -0400

    Update assignPlayerValidation.js

--- client/src/components/dashboard/players/forms/utils/assignPlayerValidation.js
+++ client/src/components/dashboard/players/forms/utils/assignPlayerValidation.js
@@ -5,13 +5,17 @@ export default function(val) {
 
 	if (!val.playerId) {
 		errors.playerId = 'Please select a player';
-	} else if (!val.team) {
+	}
+	if (!val.team) {
 		errors.team.teamId = 'Please select a team';
-	} else if (val.team && !val.team.position) {
+	}
+	if (val.team && !val.team.position) {
 		errors.team.position = 'Please choose one or more position';
-	} else if (val.team && !val.team.jerseyNum) {
+	}
+	if (val.team && !val.team.jerseyNum) {
 		errors.team.jerseyNum = 'Please select a jersey number';
-	} else if (!jerseyNumRegrex.test(val.team.jerseyNum)) {
+	}
+	if (val.team && !jerseyNumRegrex.test(val.team.jerseyNum)) {
 		errors.team.jerseyNum = 'Jersey numbers can be between 1 and 99';
 	}
 
