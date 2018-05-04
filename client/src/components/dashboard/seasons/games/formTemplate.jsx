be7b8a8c75196ad7c03f9c9354d15ae842670f4d
Dom Wakeling <domwakeling@gmail.com>
2018-05-04 01:06:30 +0100

    Prevent game having same home/away teams

--- client/src/components/dashboard/seasons/games/formTemplate.jsx
+++ client/src/components/dashboard/seasons/games/formTemplate.jsx
@@ -4,7 +4,7 @@ import Navigation from '../../helper/NavigationArrow.jsx';
 
 import RaisedButton from 'material-ui/RaisedButton';
 import MenuItem from 'material-ui/MenuItem';
-import { Field, reduxForm } from 'redux-form';
+import { Field } from 'redux-form';
 
 
 import { cssContent, cssDashboard } from '../../../styles';
@@ -13,18 +13,31 @@ import { cssContent, cssDashboard } from '../../../styles';
 import {
 	TextField,
 	SelectField,
-	DatePicker,
-	Checkbox
+	DatePicker
 } from 'redux-form-material-ui';
 
 
-const FormTemplate = props => {
-	const { handleSubmit, teams } = props;
-	
-	return (
+export default class FormTemplate extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			home: props.initialValues && props.initialValues.homeTeamId,
+			away: props.initialValues && props.initialValues.awayTeamId
+		};
+	}
+
+	handleTeamSelection(id, newTeamId, oldTeamId, fieldId) {
+		const newState = fieldId === 'home' ? {home: newTeamId} : {away: newTeamId};
+		this.setState(newState);
+	}
+
+	render() {
+		const { handleSubmit, teams, title, reset } = this.props;
+		this.handleTeamSelection = this.handleTeamSelection.bind(this);
+		return (
 			<div style={cssContent.body}>
 				<Navigation tooltip='Go Back'>
-					<h3>{props.title}</h3>
+					<h3>{title}</h3>
 				</Navigation>
 				<form
 					onSubmit={ handleSubmit }
@@ -36,7 +49,7 @@ const FormTemplate = props => {
 							component={DatePicker}
 							floatingLabelStyle={cssDashboard.formRequired}
 							floatingLabelText='Game Date'
-							format={v => v? new Date(v) : null}
+							format={v => v ? new Date(v) : null}
 							formatDate={d => d.toDateString()}
 							name='datePlayed'
 						/>
@@ -53,15 +66,20 @@ const FormTemplate = props => {
 							floatingLabelText='Home Team'
 							hintText='Select the home team'
 							name='homeTeamId'
+							onChange={(i, n, o) => {
+								this.handleTeamSelection(i, n, o, 'home');
+								}
+							}
 							style={cssDashboard.settings.forms.add.selectField}
 							>
 							{
-								teams.map(team => 
-									<MenuItem 
+								teams.filter(team => team._id !== this.state.away).map(team => (
+									<MenuItem
 										key={team._id}
 										primaryText={team.name}
 										value={team._id}
 									/>
+									)
 								)
 							}
 						</Field>
@@ -70,41 +88,46 @@ const FormTemplate = props => {
 							floatingLabelText='Opponent'
 							hintText='Select the an opponent'
 							name='awayTeamId'
+							onChange={(i, n, o) => {
+								this.handleTeamSelection(i, n, o, 'away');
+								}
+							}
 							style={cssDashboard.settings.forms.add.selectField}
 							>
 							{
-								teams.map(team => 
-									<MenuItem 
+								teams.filter(team => team._id !== this.state.home).map(team => (
+									<MenuItem
 										key={team._id}
+										// onChange={(e) => {this.handleTeamSelection(e, 'away');}}
 										primaryText={team.name}
 										value={team._id}
 									/>
+									)
 								)
 							}
-						</Field>							
+						</Field>
 					</div>
 					<RaisedButton
 						label='Submit'
 						primary={true}
-						style={{marginRight:5}}
+						style={{marginRight: 5}}
 						type='submit'
 					/>
 					<RaisedButton
 						label='Reset'
-						onTouchTap={props.reset}
+						onTouchTap={reset}
 						secondary={true}
-					/>					
+					/>
 				</form>
 			</div>
-	);
-};
+		);
+	}
+}
 
 FormTemplate.propTypes = {
 	handleSubmit: PropTypes.func,
+	initialValues: PropTypes.object,
 	reset: PropTypes.func,
+	teams: PropTypes.array,
 	title: PropTypes.string
 };
-
-export default FormTemplate;
-
-
